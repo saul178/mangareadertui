776a9ef8c5d211858962dcac4e9c7e511ee42b76
Saul Gonzalez <gonzalezsaul31@gmail.com>
2025-12-05 17:30:06 -0700

    figuring out how to string togather multiple components and manage it's state

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/saul178/mangareadertui/components/filetree"
 )
 
@@ -12,6 +13,8 @@ import (
 // this will hold all the components child models used for the main parent app to use
 // the appModel will choose which child component will be used
 type appComponents int
+
+var failedToStartTui string = "error starting mangareadertui: %v\n"
 
 const (
 	filetreeComp appComponents = iota
@@ -28,62 +31,105 @@ type mainAppModel struct {
 	// statusBar statusBar.Model
 
 	// NOTE: Main application state
-	// activeComp appComponents // tracks which component is focused on
-	// width  int
-	// height int
-	// err error // report any errors encountered
+	activeComp appComponents // tracks which component is focused on
+	width      int
+	height     int
+	err        error // report any errors encountered
 }
 
 // TODO: this will initialize all subcomponents
-func (am mainAppModel) Init() tea.Cmd {
-	ft, err := filetree.NewFileTreeModel()
-	if err != nil {
-		fmt.Printf("failed to initialize file tree component: %v\n", err)
-		return nil
-	}
-
-	// assign the models to the mainAppModel to be initialized
-	am.filetree = ft
-
-	// set the initial focus
-	// am.activeComp = filetreeComp
-
+func (mam mainAppModel) Init() tea.Cmd {
 	// batch up all the subcomp inits for the app
 	return tea.Batch(
-		am.filetree.Init(),
+		mam.filetree.Init(),
+		// am.ImageViewer etc
 	)
 }
 
 // TODO: Main application Update: Delegates messages and handles custom messages.
 // depending on which component is selected
-func (fm mainAppModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
+func (mam mainAppModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	// var cmd tea.Cmd
+	var cmds []tea.Cmd
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q", "esc":
-			return fm, tea.Quit
+			return mam, tea.Quit
+		// NOTE: idea to cycle through the components
+		case "tab":
+			mam.activeComp = (mam.activeComp + 1) % 4
+			return mam, nil
 		}
+		// NOTE:
+		// this is to resize the window of the main app but might need to send the msg down to the subcomponents too
+		// case tea.WindowSizeMsg:
+		// 	am.width = msg.Width
+		// 	am.height = msg.Height
 	}
 
-	return fm, cmd
+	// TODO: delegate the msgs to the current active component
+	switch mam.activeComp {
+	case filetreeComp:
+		newModel, newCmd := mam.filetree.Update(msg)
+		mam.filetree = newModel.(filetree.FileTreeModel) // need to type assert here
+		cmds = append(cmds, newCmd)
+		// case imageViewerComp:
+		// case searchComp:
+		// case statusBarComp:
+	}
+
+	return mam, tea.Batch(cmds...)
 }
 
 // TODO: this will define the whole view of the tui by rendering the sub components togather
-func (am mainAppModel) View() string {
-	return am.filetree.View()
+// using lipgloss for style and composition
+func (mam mainAppModel) View() string {
+	// TODO: use lipgloss for layout
+	// example: Define a style for the focused component to highlight it.
+
+	// Render file tree view
+	fileTreeView := mam.filetree.View()
+
+	// TODO: render the main content
+	// mainContentView := "No main content selected."
+	// if mam.activeComp == imageViewerComp {
+	// 	mainContentView = mam.imageViewer.View()
+	// } else if mam.activeComp == searchComp {
+	// 	mainContentView = mam.search.View()
+	// } else {
+	mainContentView := "this is a tmp str"
+	// }
+	//
+	// TODO: Combine them (using simple string concatenation for this example)
+	// In a real TUI, you would use lipgloss to tile or stack these views.
+	output := lipgloss.JoinHorizontal(
+		lipgloss.Top,
+		// File tree gets 30% width
+		lipgloss.PlaceVertical(mam.height, lipgloss.Top, fileTreeView),
+		// Main content gets the rest
+		lipgloss.PlaceVertical(mam.height, lipgloss.Top, mainContentView),
+	)
+	// add status bar at the bottom
+	// return lipgloss.JoinVertical(lipgloss.Left, output, mam.statusBar.View())
+	return output
 }
 
 func main() {
 	ft, err := filetree.NewFileTreeModel()
 	if err != nil {
-		fmt.Printf("failed to to start filetree component: %v\n", err)
-		os.Exit(1)
+		fmt.Printf("failed to initialize file tree component: %v\n", err)
 	}
-	p := tea.NewProgram(&ft)
+
+	// NOTE: not too sure about this
+	mangareadertui := mainAppModel{
+		activeComp: filetreeComp, // set default active comp
+		filetree:   ft,
+	}
+	p := tea.NewProgram(&mangareadertui) // TODO: look into other rendering options
 	if _, err := p.Run(); err != nil {
-		fmt.Println("Alas, there's been an error: ", err)
+		fmt.Printf(failedToStartTui, err)
 		os.Exit(1)
 	}
 }
